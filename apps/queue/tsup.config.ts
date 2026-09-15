0ec14f772c98eb19d58c391ce6880a329044f305
AndreiNeptune <agmatei6@gmail.com>
2026-09-15 16:56:03 +0300

    fix(queue): disable dts generation to prevent OOM on Render

--- apps/queue/tsup.config.ts
+++ apps/queue/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig((options: Options) => ({
     splitting: true,
     entry: ["src/**/*.ts", "!src/**/*.test.ts", "!src/**/__tests__/**/*.ts"],
     format: ["esm"],
-    dts: true,
+    dts: false,
     minify: true,
     clean: true,
     ...options,
